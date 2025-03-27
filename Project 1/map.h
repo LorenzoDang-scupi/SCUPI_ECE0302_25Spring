f26ed85e929af9eb2aaf6b1270d43c16385b2ff1
LorenzoDang-scupi <155616704+LorenzoDang-scupi@users.noreply.github.com>
2025-03-27 09:29:41 +0800

    Update map.h

--- Project 1/map.h
+++ Project 1/map.h
@@ -7,6 +7,8 @@
 
 // Maximum number of key/value pairs the map can hold.
 const int DEFAULT_MAX_ITEMS = 150;
+using KeyType = std::string;
+using ValueType = double;
 
 class Map
 {
@@ -14,18 +16,18 @@ public:
 	Map();         // Create an empty map (i.e., one whose size() is 0). 
 	bool empty()const;  // Return true if the map is empty, otherwise false. 
 	int size()const;    // Return the number of key/value pairs in the map. 
-	bool insert(const std::string& key, const double& value);
+	bool insert(const KeyType& key, const ValueType& value);
 	// If key is not equal to any key currently in the map and if the 
 	// key/value pair can be added to the map, then do so and return true. 
 	// Otherwise, make no change to the map and return false (indicating 
 	// that either the key is already in the map, or the map has a fixed 
 	// capacity and is full). 
-	bool update(const std::string& key, const double& value);
+	bool update(const KeyType& key, const ValueType& value);
 	// If key is equal to a key currently in the map, then make that key no 
 	// longer map to the value that it currently maps to, but instead map to 
 	// the value of the second parameter; in this case, return true. 
 	// Otherwise, make no change to the map and return false. 
-	bool insertOrUpdate(const std::string& key, const double& value);
+	bool insertOrUpdate(const KeyType& key, const ValueType& value);
 	// If key is equal to a key currently in the map, then make that key no 
 	// longer map to the value that it currently maps to, but instead map to 
 	// the value of the second parameter; in this case, return true. 
@@ -34,19 +36,19 @@ public:
 	// Otherwise, make no change to the map and return false (indicating 
 	// that the key is not already in the map and the map has a fixed 
 	// capacity and is full). 
-	bool erase(const std::string& key);
+	bool erase(const KeyType& key);
 	// If key is equal to a key currently in the map, remove the key/value 
 	// pair with that key from the map and return true.  Otherwise, make 
 	// no change to the map and return false. 
-	bool contains(const std::string& key)const;
+	bool contains(const KeyType& key)const;
 	// Return true if key is equal to a key currently in the map, otherwise 
 	// false. 
-	bool get(const std::string& key, double& value)const;
+	bool get(const KeyType& key, ValueType& value)const;
 	// If key is equal to a key currently in the map, set value to the 
 	// value in the map which the key maps to, and return true.  Otherwise, 
 	// make no change to the value parameter of this function and return 
 	// false. 
-	bool get(int i, std::string& key, double& value)const;
+	bool get(int i, KeyType& key, ValueType& value)const;
 	// If 0 <= i < size(), copy into the key and value parameters the 
 	// key and value of the key/value pair in the map whose key is strictly 
 	// greater than exactly i keys in the map and return true.  Otherwise, 
@@ -57,8 +59,8 @@ public:
 private:
 	struct Pair
 	{
-		std::string key;
-		double value =0.0;
+		KeyType key;
+		ValueType value =0.0;
 	};
 	Pair m_pairs[DEFAULT_MAX_ITEMS];
 	int m_size;
